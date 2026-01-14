d54a1ddba9418d4e7e6215d931729216b72abc0f
aswinik057 <aswinik057@gmail.com>
2026-01-14 11:41:31 -0800

    Uploading full project

---
+++ script.js
@@ -0,0 +1,15 @@
+var sidenavbar=document.querySelector(".sidenavbar")
+
+function clickme(){
+    sidenavbar.style.left="0"
+}
+
+function cancelme(){
+    sidenavbar.style.left="-60%"
+}
+
+
+
+
+
+
